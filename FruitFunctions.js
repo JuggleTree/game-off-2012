9ef2f4f3ffd4db714514bb1360bb523dd0b0b194
Steve McCarthy <raygunomix@gmail.com>
2012-11-13 08:21:44 -0500

    updated code style to use non-expanded tabs

--- FruitFunctions.js
+++ FruitFunctions.js
@@ -3,7 +3,7 @@ goog.provide('juggletest.FruitFunctions');
 var		fruitToRemove = new Array()
 	,	leftHandJoint = "empty"
 	,	rightHandJoint = "empty"
-  , heldFruit = []
+	, heldFruit = []
 	;
 
 function GenerateFruit(world, fruitLayer)
@@ -22,40 +22,40 @@ function RemoveFruit(fruit)
 
 function CatchFruit(world, fruit, hand, handType)
 {
-  //make sure fruit is not currently attached to something
-  if (!fruit.hasJoint)
-  {
-    fruit.hasJoint = true;
-    jointDef = new b2DistanceJointDef();
-    jointDef.Initialize(fruit, hand, fruit.GetPosition(), hand.GetPosition());
-    jointDef.collideConnected = true;
-    heldFruit.push([world.CreateJoint(jointDef), fruit]);
-  }
+	//make sure fruit is not currently attached to something
+	if (!fruit.hasJoint)
+	{
+		fruit.hasJoint = true;
+		jointDef = new b2DistanceJointDef();
+		jointDef.Initialize(fruit, hand, fruit.GetPosition(), hand.GetPosition());
+		jointDef.collideConnected = true;
+		heldFruit.push([world.CreateJoint(jointDef), fruit]);
+	}
 }
 
 function ThrowFruit(world)
 {
-  //fruits are currently attached
-  if (heldFruit.length > 0)
-  {
-    var temp = heldFruit.shift();
-    var currentJoint = temp[0];
-    var currentFruit = temp[1];
+	//fruits are currently attached
+	if (heldFruit.length > 0)
+	{
+		var temp = heldFruit.shift();
+		var currentJoint = temp[0];
+		var currentFruit = temp[1];
 
-    //get whichever body is the fruit
-    if (currentJoint.GetBodyA().GetUserData().type == "fruit")
+		//get whichever body is the fruit
+		if (currentJoint.GetBodyA().GetUserData().type == "fruit")
 		{
-      world.DestroyJoint(currentJoint);
-      Throw(world, currentJoint.GetBodyA());
-    }
-    else if (currentJoint.GetBodyB().GetUserData().type == "fruit")
-    {
-      world.DestroyJoint(currentJoint);
-      Throw(world, currentJoint.GetBodyA());
-    }
-    //remove joint check
-    currentFruit.hasJoint = false;
-  }
+			world.DestroyJoint(currentJoint);
+			Throw(world, currentJoint.GetBodyA());
+		}
+		else if (currentJoint.GetBodyB().GetUserData().type == "fruit")
+		{
+			world.DestroyJoint(currentJoint);
+			Throw(world, currentJoint.GetBodyA());
+		}
+		//remove joint check
+		currentFruit.hasJoint = false;
+	}
 }
 
 function Throw(world, fruit)
@@ -64,15 +64,15 @@ function Throw(world, fruit)
 	var height = mouseY - fruit.GetPosition().y;
 	if (height < 0) {height = -height;}
 	var gravity = world.GetGravity()
-	var velocityY = -Math.sqrt(2*gravity.y*height);
-	
+		var velocityY = -Math.sqrt(2*gravity.y*height);
+
 	//calculate the time to reach the apex
 	var time = -velocityY / gravity.y;
-	
+
 	//calculate the initial horizontal velocity required to hit the apex
 	var width = mouseX - fruit.GetPosition().x;
 	var velocityX = width / time;
-	
+
 	fruit.SetLinearVelocity(new b2Vec2(velocityX, velocityY));
 }
-	
+
