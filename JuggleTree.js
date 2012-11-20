3ef22c3c9bbe936691fc64fee5b66ea63564eaa2
Exor <darkex@gmail.com>
2012-11-20 21:27:57 +0900

    Game over screen starts a new game

--- JuggleTree.js
+++ JuggleTree.js
@@ -30,6 +30,9 @@ JuggleTree.start = function(debug){
 		,	buttonLayer
 		,	backgroundLayer
 		,	hudLayer
+			//Schedule Manager Functions
+		,	updateFunction
+		,	generateFruitFunction
 			//Box2d required includes
 		,   b2Vec2 = Box2D.Common.Math.b2Vec2
 		,	b2World = Box2D.Dynamics.b2World
@@ -133,69 +136,60 @@ JuggleTree.start = function(debug){
 		SetupPopupManager(hudLayer);
 		
 		//Schedule a fruit to fall every 10 seconds
-		lime.scheduleManager.scheduleWithDelay(function (dt){
+		lime.scheduleManager.scheduleWithDelay(generateFruitFunction = function (dt){
 			if (fruitLayer.getNumberOfChildren() < 3)
 				GenerateFruit(world);
 		}, director, 2000, 0);
 		
 		//This is the Update Loop
-		lime.scheduleManager.schedule(function(dt) 
+		lime.scheduleManager.schedule(updateFunction = function(dt) 
 		{
-			UpdateLoop(dt);	
+			//Update box2d
+			world.Step(dt / 1000, 8, 3);
+			world.ClearForces();
+		
+			//Remove old fruits
+			for (i=0;i<fruitToRemove.length;i)
+			{
+				var fruit = fruitToRemove.pop();
+				fruitLayer.removeChild(fruit.GetUserData().texture);
+				world.DestroyBody(fruit);
+			}
+				
+			//Add new fruits
+			for (i=0;i<fruitToAdd.length;i++)
+			{
+				var fruit = fruitToAdd.pop();
+				fruitLayer.appendChild(fruit.GetUserData().texture);
+			}
+				
+			//Draw limeJS objects
+			for (var b = world.GetBodyList(); b.GetNext()!=null; b = b.GetNext())
+			{
+				var position = b.GetPosition();
+				b.GetUserData().texture.setRotation(-b.GetAngle()/Math.PI*180);
+				b.GetUserData().texture.setPosition(position.x*30, position.y*30);
+			}
+			
+			//Update the HUD
+			scoreLbl.setText('Score: ' + points);
+			droppedLbl.setText('Dropped: ' + fruitsDropped);
+			
+			//Check for game over
+			if (fruitsDropped >= 1)
+				GameOver();
 		},director);
 		
 		director.replaceScene(gameplayScene);
 	}
 	
-	function UpdateLoop(dt)
-	{
-		//Update box2d
-		world.Step(dt / 1000, 8, 3);
-		world.ClearForces();
-	
-		//Remove old fruits
-		for (i=0;i<fruitToRemove.length;i)
-		{
-			var fruit = fruitToRemove.pop();
-			fruitLayer.removeChild(fruit.GetUserData().texture);
-			world.DestroyBody(fruit);
-		}
-			
-		//Add new fruits
-		for (i=0;i<fruitToAdd.length;i++)
-		{
-			var fruit = fruitToAdd.pop();
-			fruitLayer.appendChild(fruit.GetUserData().texture);
-		}
-			
-		//Draw limeJS objects
-		for (var b = world.GetBodyList(); b.GetNext()!=null; b = b.GetNext())
-		{
-			var position = b.GetPosition();
-			b.GetUserData().texture.setRotation(-b.GetAngle()/Math.PI*180);
-			b.GetUserData().texture.setPosition(position.x*30, position.y*30);
-		}
-		
-		//Update the HUD
-		scoreLbl.setText('Score: ' + points);
-		droppedLbl.setText('Dropped: ' + fruitsDropped);
-		
-		//Check for game over
-		if (fruitsDropped >= 1)
-			GameOver();
-	}
-	
 	function GameOver()
 	{
-		//vv this part doesn't work
-		//When game over happens all things on the schedule manager need removed.
-		lime.scheduleManager.unschedule(function(dt) 
-		{
-			UpdateLoop();	
-		},director);
-		//^^ doesn't work
+		//Remove the scheduled tasks
+		lime.scheduleManager.unschedule(updateFunction,director);
+		lime.scheduleManager.unschedule(generateFruitFunction,director);
 		
-		fruitsDropped = 0;
+		//Set up the game over screen
 		gameOverScene = new lime.Scene();
 		gameOverScene.appendChild(backgroundLayer);
 		
@@ -209,6 +203,8 @@ JuggleTree.start = function(debug){
 		gameOverScene.appendChild(startButton);
 		
 		goog.events.listen(startButton,['mousedown'],function(e){
+			//Restart the game
+			fruitsDropped = 0;
 			points = 0;
 			SetupTitleScreen();
 		});
